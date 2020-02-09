6f96dffabcc92de2c9ca63d501de33d07834ec5c
ekeskini <elias@keski-nisula.fi>
2020-02-09 14:15:10 +0200

    Function 1 preparations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,23 @@
 package main.java;
 
 import java.sql.*;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) throws SQLException {
+		Connection parceldb = DriverManager.getConnection("jdbc:sqlite:parceldatabase.db");
+		createDatabase(parceldb);
 		
+	}
+	
+	public static void createDatabase(Connection connection){
+		try {
+			Statement s = connection.createStatement();
+			s.execute("CREATE TABLE Asiakas (id INTEGER PRIMARY KEY, name TEXT UNIQUE)");
+			
+		} catch (SQLException e) {
+			System.out.println("Database already exists");
+		}
 	}
 }
