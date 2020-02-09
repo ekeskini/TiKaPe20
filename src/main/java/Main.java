31e114a266b76a123242bb7270a48b348ef68584
ekeskini <elias@keski-nisula.fi>
2020-02-09 12:55:42 +0200

    JDBC driver setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 package main.java;
 
+import java.sql.*;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world");
+	public static void main(String[] args) throws SQLException {
+		
 	}
-
 }
